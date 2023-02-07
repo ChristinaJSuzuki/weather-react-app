6896a0f081ae05848365a0d50722e65fac32bebf
Christina Suzuki <christina.suzuki0430@gmail.com>
2023-02-07 10:16:18 -0800

    removed react loader spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -12,15 +11,4 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
-  return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
-  );
 }
