0ea302941d931959092b5900717761932e35aa32
Christina Suzuki <christina.suzuki0430@gmail.com>
2023-02-06 18:19:47 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello from React</p>
+        <Weather city="Tokyo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
