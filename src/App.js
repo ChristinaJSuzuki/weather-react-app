97cbe888e7d6a4f1602d628e38c84d08d5dc7a13
Christina Suzuki <christina.suzuki0430@gmail.com>
2023-02-07 23:25:31 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,102 +4,104 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <div class="vanilla-weather-app-wrapper">
-        <div class="container">
-          <div class="parent">
-            <div class="card" style={{ width: "50rem" }}>
-              <form class="search-form" id="search-form">
-                <div
-                  class="box"
-                  id="search-form"
-                  style={{
-                    eight: "30px",
-                    width: "235px",
-                    margin: "10px 230px 0px",
-                  }}
-                >
-                  <input
-                    type="search"
-                    placeholder="Enter Location"
-                    autofocus="on"
-                    autocomplete="off"
-                    id="city-input"
-                  />
-                  <input type="submit" value="Go" />
-                </div>
-              </form>
-              <div
-                class="card-body"
-                style={{ width: "797px", height: "526px" }}
-              >
-                <div class="london">
-                  <h1 class="card-title" id="city">
-                    London, England
-                  </h1>
-                </div>
-                <h1 class="card-text">
-                  <div class="current-time">
-                    <p>
-                      Last updated: <span id="date"></span>
-                    </p>
-                  </div>
-                  <div class="col-6">
-                    <ul>
-                      <li id="description"></li>
-                      <li>
-                        Humidity: <span id="humidity"></span>%
-                      </li>
-                      <li>
-                        Wind: <span id="wind"></span>km/h
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="ninedegrees">
-                    <span class="temperature" id="temperature">
-                      9
-                    </span>
-                    <span class="units" style={{ fontSize: "18pt" }}>
-                      <a
-                        href="_blank"
-                        id="celsius-link"
-                        className="text-decoration-none"
-                      >
-                        °C{" "}
-                      </a>
-                      |
-                      <a
-                        href="_blank"
-                        id="fahrenheit-link"
-                        className="text-decoration-none"
-                      >
-                        °F
-                      </a>
-                    </span>
-                  </div>
-                  <div className="littlestars">
-                    <img
-                      src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                      alt="Clear"
-                      id="icon"
-                      className="float-left"
-                    />
-                  </div>
-                </h1>
-                <div class="card-group" id="forecast">
+      <div className="container">
+        <div class="vanilla-weather-app-wrapper">
+          <div class="container">
+            <div class="parent">
+              <div class="card" style={{ width: "50rem" }}>
+                <form class="search-form" id="search-form">
                   <div
-                    class="card-0"
-                    style={{ width: "9.5rem", height: "7.3rem" }}
+                    class="box"
+                    id="search-form"
+                    style={{
+                      eight: "30px",
+                      width: "235px",
+                      margin: "10px 230px 0px",
+                    }}
                   >
-                    <div class="card-body-mon">
-                      <h5 class="card-title-mon">MON</h5>
+                    <input
+                      type="search"
+                      placeholder="Enter Location"
+                      autofocus="on"
+                      autocomplete="off"
+                      id="city-input"
+                    />
+                    <input type="submit" value="Go" />
+                  </div>
+                </form>
+                <div
+                  class="card-body"
+                  style={{ width: "797px", height: "526px" }}
+                >
+                  <div class="london">
+                    <h1 class="card-title" id="city">
+                      London, England
+                    </h1>
+                  </div>
+                  <h1 class="card-text">
+                    <div class="current-time">
+                      <p>
+                        Last updated: <span id="date"></span>
+                      </p>
+                    </div>
+                    <div class="col-6">
+                      <ul>
+                        <li id="description"></li>
+                        <li>
+                          Humidity: <span id="humidity"></span>%
+                        </li>
+                        <li>
+                          Wind: <span id="wind"></span>km/h
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="ninedegrees">
+                      <span class="temperature" id="temperature">
+                        9
+                      </span>
+                      <span class="units" style={{ fontSize: "18pt" }}>
+                        <a
+                          href="_blank"
+                          id="celsius-link"
+                          className="text-decoration-none"
+                        >
+                          °C{" "}
+                        </a>
+                        |
+                        <a
+                          href="_blank"
+                          id="fahrenheit-link"
+                          className="text-decoration-none"
+                        >
+                          °F
+                        </a>
+                      </span>
+                    </div>
+                    <div className="littlestars">
                       <img
-                        src="http://openweathermap.org/img/wn/50d@2x.png"
-                        alt=""
-                        width="36"
+                        src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                        alt="Clear"
+                        id="icon"
+                        className="float-left"
                       />
-                      <div class="weather-forecast-temperatures">
-                        <span class="card-text-degree-max"> 17° </span>
-                        <span class="card-text-degree-min"> 12° </span>
+                    </div>
+                  </h1>
+                  <div class="card-group" id="forecast">
+                    <div
+                      class="card-0"
+                      style={{ width: "9.5rem", height: "7.3rem" }}
+                    >
+                      <div class="card-body-mon">
+                        <h5 class="card-title-mon">MON</h5>
+                        <img
+                          src="http://openweathermap.org/img/wn/50d@2x.png"
+                          alt=""
+                          width="36"
+                        />
+                        <div class="weather-forecast-temperatures">
+                          <span class="card-text-degree-max"> 17° </span>
+                          <span class="card-text-degree-min"> 12° </span>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -107,18 +109,18 @@ function App() {
               </div>
             </div>
           </div>
+          <span className="github-link">
+            <a
+              href="https://github.com/ChristinaJSuzuki/weather-react-app.git"
+              target="_blank"
+              rel="noreferrer"
+              className="text-decoration-none"
+            >
+              Open-source code
+            </a>{" "}
+            by Christina J Suzuki
+          </span>
         </div>
-        <span className="github-link">
-          <a
-            href="https://github.com/ChristinaJSuzuki/weather-react-app.git"
-            target="_blank"
-            rel="noreferrer"
-            className="text-decoration-none"
-          >
-            Open-source code
-          </a>{" "}
-          by Christina J Suzuki
-        </span>
       </div>
     </div>
   );
